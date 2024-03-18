eaaa229c242da63fe18ae50a3302ba2f7ccefc3d
DanyC <danielcrangasu@gmail.com>
2024-03-18 16:30:52 +0200

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,42 @@
+# simple calculator made in python
+# added a while loop to better simulate a traditional calculator
+
+on = True
+def add():
+    a = float(input("Enter a number:"))
+    b = float(input("Enter a second number:"))
+    print("Result is: {}".format(a+b))
+
+
+def subtraction():
+    a = float(input("Enter a number:"))
+    b = float(input("Enter a second number:"))
+    print(a-b)
+
+
+def multiply():
+    a = float(input("Enter a number:"))
+    b = float(input("Enter a second number:"))
+    print(a*b)
+
+
+def divide():
+    a = float(input("Enter a number:"))
+    b = float(input("Enter a second number:"))
+    print(a/b)
+
+while on:
+    print("Calculator:")
+    operation = input("Choose the operation: + , - , * , / , q")
+    if operation == "+":
+        add()
+    elif operation == "-":
+        subtraction()
+    elif operation == "*":
+        multiply()
+    elif operation == "/":
+        divide()
+    elif operation == "q":
+        print("Calculator app has been stopped.")
+        on = False
+    else: print("This is not available.")
